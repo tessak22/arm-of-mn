93132b319de893b6d6a41f3171accd0cc045af5c
Tessa Kriesel <tessak22@outlook.com>
2015-09-09 16:55:42 -0500

    Add call for rand-bg script

--- templates/armofmn/index.php
+++ templates/armofmn/index.php
@@ -207,6 +207,12 @@ else
 	<script src="templates/<?php echo $this->template ?>/js/jquery.js"></script>
 	<script src="templates/<?php echo $this->template ?>/js/script.js"></script>
   <script src="templates/<?php echo $this->template ?>/js/rand-bg.js"></script>
+  <script>
+    $(".randbg").RandBG({
+      ClassPrefix: "bg",
+      count: 9
+    });
+  </script>
 	<!-- end scripts-->
 
 </body>
